b7769d97cc4cd8dd454253c27d5e08d36d363531
okanbeydanol <60093192+okanbeydanol@users.noreply.github.com>
2023-12-18 16:49:52 +0300

    test

--- src/ios/GooglePlus.m
+++ src/ios/GooglePlus.m
@@ -56,7 +56,7 @@
         } else {
             NSString *email = user.profile.email;
             NSString *userId = user.userID;
-            NSString *accessToken = user.accessToken;
+            NSString *accessToken = user.authentication.accessToken;
             NSURL *imageUrl = [user.profile imageURLWithDimension:120]; // TODO pass in img size as param, and try to sync with Android
             NSDictionary *result = @{
                            @"accessToken"     : accessToken,
